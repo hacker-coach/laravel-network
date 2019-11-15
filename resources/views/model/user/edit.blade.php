e2402a9dac0fd6e19b6c070f21b51d6af8b0c3de
mbarthel <mba@webmen.de>
2019-11-15 21:15:51 +0100

    xx

--- resources/views/model/user/edit.blade.php
+++ resources/views/model/user/edit.blade.php
@@ -14,18 +14,18 @@
                         <form method="POST" action="{{ route('userUpdate') }}" enctype="multipart/form-data">
                             @csrf
 
-                            @include('form.checkbox', ['title' =>__('Profil aktivieren für das Netzwerk'),'name'=>'show_profil','value'=>$user->show_profil])
-                            @include('form.checkbox', ['title' =>__('Öffentliches Profil im WWW'),'name'=>'show_profil_www','value'=>$user->show_profil_www])
-                            @include('form.checkbox', ['title' =>__('Profil für Firmen anzeigen'),'name'=>'show_profil_for_company','value'=>$user->show_profil_for_company])
+                            @include('form.checkbox', ['title' =>__('Profil aktivieren für das Netzwerk'),'name'=>'show_profil','value'=> old('show_profil', $user->show_profil)])
+                            @include('form.checkbox', ['title' =>__('Öffentliches Profil im WWW'),'name'=>'show_profil_www','value'=> old('show_profil_www', $user->show_profil_www)])
+                            @include('form.checkbox', ['title' =>__('Profil für Firmen anzeigen'),'name'=>'show_profil_for_company','value'=>old('show_profil_for_company', $user->show_profil_for_company)])
 
                             @include('form.text', ['title' =>__('Name'),'name'=>'name','value'=>$user->name,'required'=>'required'])
                             @include('form.text', ['title' =>__('Slogan'),'name'=>'slogan','value'=>$user->slogan,'required'=>'required'])
 
                             @include('form.upload', ['title' =>__('Quadrat-Image'),'name'=>'image'])
 
-                            @include('form.text', ['title' =>__('Xing'),'name'=>'xing','value'=>$user->xing,'required'=>''])
-                            @include('form.text', ['title' =>__('Linkedin'),'name'=>'linkedin','value'=>$user->linkedin,'required'=>''])
-                            @include('form.text', ['title' =>__('www'),'name'=>'www','value'=>$user->www,'required'=>''])
+                            @include('form.text', ['title' =>__('Xing'),'name'=>'xing','value'=>old('xing', $user->xing),'required'=>''])
+                            @include('form.text', ['title' =>__('Linkedin'),'name'=>'linkedin','value'=>old('linkedin', $user->linkedin),'required'=>''])
+                            @include('form.text', ['title' =>__('www'),'name'=>'www','value'=> old('www', $user->www),'required'=>''])
 
 
 
