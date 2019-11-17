083f74de81056826d56c03e64a30fc39d9992b49
mbarthel <mba@webmen.de>
2019-11-17 12:50:02 +0100

    xx

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,11 +24,23 @@
 <!-- Page Content -->
 <div class="container" style="padding-top: 2rem !important;  padding-bottom: 2rem;">
     <div class="row">
-        @foreach ($users as $user)
-            <div class="col-xl-3 col-md-6 mb-4">
-                @include('snippet.card')
-            </div>
-        @endforeach
+        @if (true)
+            @guest
+                <div class="alert alert-warning" role="alert">
+                    Sie müssen registrieren Sie sich sich!
+                </div>
+            @else
+                <div class="alert alert-warning" role="alert">
+                    Es sind keine Profile öffentlich geschaltet!
+                </div>
+            @endguest
+        @else
+            @foreach ($users as $user)
+                <div class="col-xl-3 col-md-6 mb-4">
+                    @include('snippet.card')
+                </div>
+            @endforeach
+        @endif
     </div>
     <!-- /.row -->
 </div>
