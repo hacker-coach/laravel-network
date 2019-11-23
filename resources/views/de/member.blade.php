585c88ba7fba4b84e783d7c79dd5d1293a9fc60f
mbarthel <noreply@buntvogel.de>
2019-11-23 12:07:24 +0100

    new1

--- resources/views/de/member.blade.php
+++ resources/views/de/member.blade.php
@@ -2,28 +2,7 @@
 
 @include('snippet.nav')
 
-
-<header class="header-col3  text-white " style="background-color: #f82249;" >
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-2 ">
-            </div>
-            <div class="col-md-5 header-col3-banner" style="color:white;">
-                <h2 class="display-4 " style="text-transform: lowercase;font-family: serif;">Mitglieder</h2>
-                <div style="padding-top:15px;padding-right:25px;padding-bottom:5px;">
-                    @guest
-                        <a class="btn btn-dark btn-xl" style="text-transform: lowercase;font-family: monospace;border-radius: 10rem; border: 5px solid #343a40;"  href="{{ route('register') }}">
-                         Kostenlose Anzeigen schalten  </a>
-                     @endguest
-                </div>
-            </div>
-            <div class="col-md-4 header-col3-list" style="color:white;">
-            </div>
-            <div class="col-md-1 ">
-            </div>
-        </div>
-    </div>
-</header>
+@include('snippet.sectionheader', ['title' =>'Mitglieder'])
 
 
 <!-- Page Content -->
