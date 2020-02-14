470edbf01defd4db50650990c32906d18b652827
Your Name <you@example.com>
2020-02-14 19:31:39 +0100

    user

--- resources/views/de/concept.blade.php
+++ resources/views/de/concept.blade.php
@@ -27,6 +27,29 @@
             </div>
             <div class="col-md-8">
 
+                <h1 class="black-box"> Konzept - Mitglieder Portal</h1>
+
+                <h2 class="black-box"> Mitglied werden </h2>
+<p class="lead mb-0">                 Alle können ein Mitglied werden und sich registrieren. Mindestens 10 Voll-Mitglieder müssen
+    für das neue Mitglied eine persönliche Empfehlung abgeben.
+    Alle Mitglieder müssen sich untereinander kennen. Alle Voll-Mitglieder müssen mindestens ein Jahr im Netzwerk aktiv tätig gewesen sein.</p>
+
+
+                <h2 class="black-box">Normales Mitglied</h2>
+                <p class="lead mb-0">                Ein normales Mitglied kann nur ein Profil im Mitglieder-Bereich erstellen. Kann aber keine Mitglieder des
+                    Mitglieder-Bereiches sehen.</p>
+
+
+                    <h2 class="black-box"> Voll-Mitglied</h2>
+                <p class="lead mb-0"> Ein Voll-Mitglied kann alle Profile sehen und sein Profil außerhalb des Mitglieder-Bereiches sichtbar machen.</p>
+
+
+                        <h2 class="black-box"> Firmen</h2>
+                <p class="lead mb-0">                Firmen können 3 kostenlose Job-Anzeigen schalten. Gegen Bezahlung kann ein Problem im Mitglieder-Bereich
+                    publiziert werden. Die Mitglieder werden darüber informiert.
+                    Gegen Bezahlung kann eine Firma die Job-Anzeigen außerhalb des Mitglieder-Bereiches sichtbar machen.</p>
+
+
 
 
             </div>
