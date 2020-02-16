17ef6d4018b8e6f12b7a579afb7e7858a187416e
Your Name <you@example.com>
2020-02-16 17:37:58 +0100

    user

--- resources/views/de/concept.blade.php
+++ resources/views/de/concept.blade.php
@@ -25,43 +25,43 @@
         <h1 class="black-box"> Konzept - Mitglieder Portal</h1>
         <div class="row">
             <div class="col-md-6">
-                <h2 class="black-box"> Problemlöser (PS) werden </h2>
+                <br><h2 class="black-box"> Problemlöser (PS) werden </h2>
                 <p class="lead mb-0">
                     Alle können PS-Mitglied werden und sich registrieren. Mindestens 10 Voll-PS-Mitglieder müssen
                     für das neue PS-Mitglied eine persönliche Empfehlung abgeben.
                     Alle PS-Mitglieder müssen sich untereinander kennen. Alle Voll-PS-Mitglieder müssen mindestens ein Jahr im Netzwerk aktiv tätig gewesen sein.
                 </p>
-                <h3 class="black-box">Normales PS-Mitglied</h3>
+                <h4 class="black-box">Normales PS-Mitglied</h4>
                 <p class="lead mb-0">Ein normales PS-Mitglied kann nur ein Profil im Mitglieder-Bereich erstellen. Kann aber keine Mitglieder des
                     Mitglieder-Bereiches sehen.
                 </p>
                 <h3 class="black-box"> Voll-Mitglied</h3>
                 <p class="lead mb-0"> Ein Voll-Mitglied kann alle Profile sehen.
                 </p>
-                <h3 class="black-box">Privatsphäre</h3>
+                <h4 class="black-box">Privatsphäre</h4>
                 <p class="lead mb-0">
                     Als PS-Mitglied darf man keine Privaten-Daten wie E-Mail und echten Namen im Netzwerk angeben.
                     Das Ziel ist es, dass sich jedes PS-Mitglied mit seinen wahren inividuellen Talenten öffnet und
                     sich durch den Markt in keine Rolle gedrängt wird.
                 </p>
-                <h3 class="black-box">Kommunikation der Mitglieder</h3>
+                <h4 class="black-box">Kommunikation der Mitglieder</h4>
                 <p class="lead mb-0">
                     Mitglieder können sich über öffentliche Treffen persönlich kennenlernen.
-                    Über verschlüsselte Messanger können PS-Mitglied kommunieren. Aus Sicherheitsgründen läuft diese Kommunikation nicht über diese Plattform.
+                    Über verschlüsselte Messanger können PS-Mitglied kommunizieren. Aus Sicherheitsgründen läuft diese Kommunikation nicht über diese Plattform.
                 </p>
             </div>
             <div class="col-md-6">
-                <h2 class="black-box">Firmen</h2>
+                <br><h2 class="black-box">Firmen</h2>
                 <p class="lead mb-0">Firmen können 3 kostenlose Job-Anzeigen schalten. Gegen Bezahlung kann ein Problem im Mitglieder-Bereich
                     publiziert werden. Die Mitglieder werden darüber informiert.
                     Gegen Bezahlung kann eine Firma die Job-Anzeigen außerhalb des Mitglieder-Bereiches sichtbar machen.
                 </p>
             </div>
         </div>
-        <br><br><br>
+
         <div  class="row">
             <div class="col-md-6">
-                <h2 class="black-box">Vermittler</h2>
+                 <br><h2 class="black-box">Vermittler</h2>
                 <p class="lead mb-0">
                     Alle Vermittler müssen Voll-PS-Mitglied sein. Nur Vermittler habe Zugriff auf alle Daten.
                     Als Vermittler hat man Zugriff auf eine Extra-Datenbank mit allen Firmen-Ansprechpartnern.
@@ -70,7 +70,7 @@
                     entweder die Informationen über die Telefonate teilen für Andere Vermittler oder einen neuen Kontakt hinzufügen.
                     Man verliert den Zugriff auf die Kontakte, wenn man innerhalb eines Monats keine neues Informationen zum Netzwerk hinzugefügt hat.
                 </p>
-                <h2 class="black-box">Vermittlung</h2>
+                <h4 class="black-box">Vermittlung</h4>
                 <p class="lead mb-0">
                     Für die Vermittlung eines PS-Mitglieds wird bei der Firma Geld verlangt. 25% dieses Geldes wird in den Ausbau des Netzwerkes investiert.
                     Die Firma verpflichtet sich regelmäßig alle 2 Monate an Coachings für die Viefalt in Unternehmen (Diversity-Management) zu buchen für
@@ -79,7 +79,7 @@
                 </p>
             </div>
             <div class="col-md-6">
-                <h2 class="black-box">Fan</h2>
+                <br><h2 class="black-box">Fan</h2>
                 <p class="lead mb-0">
                     Als Fan kann man ein öffentliches Profil mit Bild, Slogan und Links erstellen. Ein Fan-Profil ist ein Extra-Account der nicht mit PS-Profil verknüpft ist.
                 </p>
