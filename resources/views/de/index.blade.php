b3af40edda0b253815b1e12ca8394193a70fbec5
Your Name <you@example.com>
2020-03-01 20:38:58 +0100

    app database resource vendor

--- resources/views/de/index.blade.php
+++ resources/views/de/index.blade.php
@@ -108,7 +108,7 @@
                 <h3 class="black-box">Können wir jedes Problem lösen?</h3>
                 <p class="lead mb-0">
                     Natürlich können wir nie wissen, ob wir Ihr Problem wirklich lösen können. Allerdings trauen wir uns zu, dass wir jedes Problem lösen können. Egal wie schwer.
-                    Wir sind ein starkes Team an Denkern. Als Autodidakten und Top-Experten haben wir schon viele verrückte unbekannte Probleme gelöst.
+                    Wir sind ein starkes Team an Denkern. Als Autodidakten und Top-Experten haben wir schon viele verrückte Probleme gelöst.
                 </p>
 
             </div>
