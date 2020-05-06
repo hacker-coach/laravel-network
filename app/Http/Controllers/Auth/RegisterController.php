c8f4e672cfdf7e194f54841b93cf73b63041d3af
Your Name <you@example.com>
2020-05-06 22:40:39 +0200

    app database resource vendor

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -68,6 +68,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'code' => ['required', new AccessCode($this->code)],
+            'role' => ['required', new AccessCode('role_hunter')],
             'dgsvo' => 'accepted',
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
@@ -82,7 +83,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        if($data['code']===$this->code){
+        if($data['code']===$this->code && $data['role']==='role_hunter'){
                 $user = User::create($this->renderData($data));
                 Mail::to('log@problemsolvernetwork.org')->send(new VerificationMail($user,'RegisterController::create'));
                 return $user;
